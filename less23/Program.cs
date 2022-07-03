63823f6dac2c041b60c9b6ac6abc790a337f8743
Dmitry Svyatov <dimka201993@gmail.com>
2022-07-03 17:11:13 +0300

    Задача кубы числа N

---
+++ less23/Program.cs
@@ -0,0 +1,20 @@
+﻿int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int cub(int number)
+{
+    return number*number*number;
+}
+
+int N= InputInt("Ведите число N=");
+
+int count = 1;
+while(count<=N)
+{
+     int tbl = cub(count);
+    Console.Write(tbl + "," );
+    count++;
+} 
